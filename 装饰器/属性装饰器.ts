7aa0b523013ef9fd0659040a946cd5e1969540f8
Leslie <100113408@qq.com>
2017-04-21 00:29:55 +0800

    使用webstorm提交，内容为参数装饰器

--- 装饰器/属性装饰器.ts
+++ 装饰器/属性装饰器.ts
@@ -1,11 +1,11 @@
 /**
  * Created by 周婷 on 2017/4/20.
  */
-class Student{
+class Student {
     @test name: string
 }
 
-function test(target: any, propertyKey: string|symbol){
+function test(target: any, propertyKey: string | symbol) {
     console.log(target);// Student {}
     console.log(propertyKey); // name
 }
